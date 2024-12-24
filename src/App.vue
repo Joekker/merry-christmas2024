e6350c634dc37a27b6a8f3d2f57f7e25a905f8c2
Joekker <57929780+redifinition@users.noreply.github.com>
2024-12-25 01:07:16 +0800

    燃放

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import AboutView from './views/AboutView.vue';
+</script>
+
+<template>
+  <div id="app">
+    <AboutView />
+    <!-- <router-view/> -->
+  </div>
+</template>
+
+<style>
+
+</style>
